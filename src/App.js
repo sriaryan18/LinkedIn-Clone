49cdd8b8e62f8efe74ff202a8608ccc63ecc15b6
Kuliza-506 <aryan.shrivastava@kuliza.com>
2023-07-01 20:30:42 +0530

    linkedIn clone self

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes,Route, Link } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav>
+      <ul>
+        <li>
+          <Link to = "/">Home</Link>          
+        </li>
+        <li>
+        <Link to = "login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+     <Routes>
+        <Route  path='/' element={<h1>Hi This is homepage</h1>}/>
+        <Route path='login' element={<Login/>}/>
+     </Routes>
+     </>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+
+const Login = (props)=>{
+    console.log("scsin");
+    return (
+        <h1>Login</h1>
+    );
+}
+
+export default Login;
